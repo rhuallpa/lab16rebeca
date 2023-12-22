d4dc489f4133700338831c1fd6646c44250dcd80
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2023-12-21 21:46:01 -0500

    Agregar archivos de proyecto.

---
+++ lab16rebeca/lab16rebeca/Models/ResponseBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab16rebeca.Models
+{
+    public class ResponseBase
+    {
+       public List<PlaceModel> places { get; set; }
+    }
+}
